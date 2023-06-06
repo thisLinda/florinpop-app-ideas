69ad6149647274015b8119659dbe87508d0ec671
Linda Forlizzi <la.for.lizzi@gmail.com>
2023-06-06 12:26:02 -0400

    clean up stray ; & other weirdness

--- BinaryToDecimal/script.js
+++ BinaryToDecimal/script.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-  let binaryInput = document.getElementById("binaryInput");
-  let decimalOutput = document.getElementById("decimalOutput");
+  let binaryInput = document.getElementById("binaryInput")
+  let decimalOutput = document.getElementById("decimalOutput")
 
   binaryInput.addEventListener("input", function(e) {
-    let binaryValue = binaryInput.value;
+    let binaryValue = binaryInput.value
     // if the binary value is valid (consists of 0's and 1's only)
-    console.log(`binaryValue: ${binaryValue}`)
     if (/^[01]{1,8}$/.test(binaryValue)) {
-      let decimalValue = parseInt(binaryValue, 2);
+      let decimalValue = parseInt(binaryValue, 2)
       decimalOutput.value = decimalValue
     } else {
       decimalOutput.decimalValue = ""
     }
-  });
-});
+  })
+})
