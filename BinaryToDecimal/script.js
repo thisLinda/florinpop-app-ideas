ab777844a31b93cd7f36e68943e689020515af2d
Linda Forlizzi <la.for.lizzi@gmail.com>
2023-06-06 10:48:10 -0400

    code running as expected

--- BinaryToDecimal/script.js
+++ BinaryToDecimal/script.js
@@ -1,27 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-  // Get the binary input field and decimal output box elements
   let binaryInput = document.getElementById("binaryInput");
-  // let decimalOutput = document.getElementById("decimalOutput");
-  let displayOutput = document.getElementsByName("displayOutput")
+  let decimalOutput = document.getElementById("decimalOutput");
 
-  // Add event listener to binary input field
   binaryInput.addEventListener("input", function(e) {
-    // Get the binary input value
     let binaryValue = binaryInput.value;
-
-    // Check if the binary value is valid (consists of 0's and 1's only)
+    // if the binary value is valid (consists of 0's and 1's only)
+    console.log(`binaryValue: ${binaryValue}`)
     if (/^[01]{1,8}$/.test(binaryValue)) {
-      // Convert the binary value to decimal
       let decimalValue = parseInt(binaryValue, 2);
-
-      // Display the decimal value inside the output box
-      // decimalOutput.textContent = decimalValue;
-      displayOutput.textContent = decimalValue
-      console.log(decimalValue)
+      decimalOutput.value = decimalValue
     } else {
-      // Clear the decimal value if the binary value is invalid
-      // decimalOutput.textContent = "";
-      displayOutput.textContent = ""
+      decimalOutput.decimalValue = ""
     }
   });
 });
